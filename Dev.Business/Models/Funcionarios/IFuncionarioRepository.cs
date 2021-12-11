f07876de3e882d38145ccefa44b2be520a9afb20
Gabriel Lacroce <gp.lacroce@gmail.com>
2021-12-11 13:24:49 -0300

    sequencia

---
+++ Dev.Business/Models/Funcionarios/IFuncionarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dev.Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Dev.Business.Models.Funcionarios
+{
+    public interface IFuncionarioRepository : IRepository<Funcionario>
+    {
+        Task<Funcionario> ObterFuncionarioPorEndereco(Guid id);
+        Task<Funcionario> ObterFucionarioPontosEndereco(Guid id);
+
+    }
+}
