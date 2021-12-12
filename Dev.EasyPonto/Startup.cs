493a5474ba5689abc50a23a3fe0d0061b051b5ee
Gabriel Lacroce <gp.lacroce@gmail.com>
2021-12-12 09:05:03 -0300

    Atualização Proj MVC

---
+++ Dev.EasyPonto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dev.EasyPonto.Startup))]
+namespace Dev.EasyPonto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Dev.Infra/Data/Context/EasyPontoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Dev.Business.Models.Funcionarios;
+using Dev.Business.Models.Pontos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.Infra.Data.Context
+{
+    public class EasyPontoDbContext : DbContext
+    {
+
+        public EasyPontoDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Ponto> Pontos { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+    }
+}
