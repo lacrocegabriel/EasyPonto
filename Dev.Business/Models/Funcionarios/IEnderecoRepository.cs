582ac0b51f46b9c71e653fe37dd36fce508b04eb
Gabriel Lacroce <gp.lacroce@gmail.com>
2021-12-12 08:56:23 -0300

    Criaão de Repositorios

---
+++ Dev.Business/Models/Funcionarios/IEnderecoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Dev.Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Dev.Business.Models.Funcionarios
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+
+    }
+}
---
+++ Dev.Business/Models/Pontos/IPontoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dev.Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dev.Business.Models.Pontos
+{
+    public interface IPontoRepository :  IRepository<Ponto>
+    {
+        Task<IEnumerable<Ponto>> ObterPontosPorFuncionario(Guid funcionarioId);
+        Task<IEnumerable<Ponto>> ObterPontosFuncionarios();
+        Task<Ponto> ObterPontoFuncionario(Guid id);
+    }
+}
